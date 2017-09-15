5495edcc725c128ce64ffb2309f90f169cee2186
Yinka Ash <developer@designsbynetguru.com>
2017-09-16 01:12:17 +0800

    Create Nav-tabs section on Index

--- index.php
+++ index.php
@@ -42,7 +42,82 @@ include('config/db.php');
     <![endif]-->
 
     <!-- Start coding here -->
+	
+	<nav class="navbar navbar-default" role="navigation">
+	<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#">CRUDDIE</a>
+		</div>
 
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse navbar-ex1-collapse">
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="#">Link</a></li>
+				<li><a href="#">Link</a></li>
+			</ul>
+			<form class="navbar-form navbar-left" role="search">
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Search">
+				</div>
+				<button type="submit" class="btn btn-default">Submit</button>
+			</form>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#">Link</a></li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+					<ul class="dropdown-menu">
+						<li><a href="#">Action</a></li>
+						<li><a href="#">Another action</a></li>
+						<li><a href="#">Something else here</a></li>
+						<li><a href="#">Separated link</a></li>
+					</ul>
+				</li>
+			</ul>
+		</div><!-- /.navbar-collapse -->
+	</nav>
+
+	
+    <div class="container">
+    	<ul class="nav nav-tabs">
+    		<li>
+    			<a href="#index" data-toggle="tab">Home</a>
+    		</li>
+    		
+    		<li>
+    			<a href="#login" data-toggle="tab">Log In</a>
+    		</li>
+    		<li>
+    			<a href="#signup" data-toggle="tab">Register</a>
+    		</li>
+    		<li>
+    			<a href="#admin" data-toggle="tab">Admin Area</a>
+    		</li>
+    	</ul>
+    	
+    	<div class="tab-content">
+    		<div id="index" class="tab-pane fade in">
+    			Home
+    		</div>
+    		
+    		<div id="login" class="tab-pane fade in">
+    			Login
+    		</div>
+    		
+    		<div id="signup" class="tab-pane fade in">
+    			Register
+    		</div>
+    		
+    		<div id="admin" class="tab-pane fade in">
+    			Admin Area
+    		</div>
+    	</div>
+    </div>
 
     <!-- Coding End -->
 
