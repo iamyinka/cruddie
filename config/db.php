bb1f16dc909c5d99826d1d90170cfb86ab08cf6c
Yinka Ash <developer@designsbynetguru.com>
2017-09-16 00:47:58 +0800

    Create Output Buffer, Session & DB Connection

--- config/db.php
+++ config/db.php
@@ -1,11 +1,22 @@
-<?php 
+<?php
+
+//Create Output Buffer
+ob_start();
+
+
+//Create Session
+
+if(!isset($_SESSION)) {
+	session_start();
+}
+
+
+// Connect to DB
 
 $connection = mysqli_connect("localhost", "netguru", "success12", "cruddie");
 
 if(mysqli_connect_errno()) {
 	echo "Error: " . mysqli_connect_error();
-} else {
-	echo "Connection to DB successful!";
 }
 
 ?>
